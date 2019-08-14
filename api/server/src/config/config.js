1e08779ba0d0dbe9ded5537cccb3551cc54e91c7
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-08-14 15:58:01 +0100

    editing config.js and creating a .env file

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,26 +1,30 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
+require('dotenv').config();
+
+module.exports =
+    /*{if using online databases 
+      development :{
+        use_env_variable: 'DATABASE_URL'
+      },*/
+    {
+        "development": {
+            "username": "postgres",
+            "password": null,
+            "database": "books",
+            "host": "127.0.0.1",
+            "dialect": "postgres",
+        },
+        "test": {
+            "username": "postgres",
+            "password": null,
+            "database": "book_test",
+            "host": "127.0.0.1",
+            "dialect": "postgres"
+        },
+        "production": {
+            "username": process.env.DB_USER,
+            "password": process.env.DB_PASS,
+            "database": process.env.DB_NAME,
+            "host": process.env.DB_HOST,
+            "dialect": 'postgres'
+        }
+    }
